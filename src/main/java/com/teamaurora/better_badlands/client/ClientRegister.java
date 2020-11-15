e3aca730562aa1aab001bed44776350196e589a8
Gkoliver <37561701+TrueGkoliver@users.noreply.github.com>
2020-11-15 09:26:51 -0600

    Fixing client registry

--- src/main/java/com/teamaurora/better_badlands/client/ClientRegister.java
+++ src/main/java/com/teamaurora/better_badlands/client/ClientRegister.java
@@ -18,9 +18,9 @@ public class ClientRegister {
     }
 
     private static void setupRenderLayer() {
-        RenderTypeLookup.setRenderLayer(BetterBadlandsBlocks.DEAD_BUSH_THATCH.get(), RenderType.getCutoutMipped());
-        RenderTypeLookup.setRenderLayer(BetterBadlandsBlocks.DEAD_BUSH_THATCH_SLAB.get(), RenderType.getCutoutMipped());
-        RenderTypeLookup.setRenderLayer(BetterBadlandsBlocks.DEAD_BUSH_THATCH_STAIRS.get(), RenderType.getCutoutMipped());
-        RenderTypeLookup.setRenderLayer(BetterBadlandsBlocks.DEAD_BUSH_THATCH_VERTICAL_SLAB.get(), RenderType.getCutoutMipped());
+        RenderTypeLookup.setRenderLayer(BetterBadlandsBlocks.KINDLING.get(), RenderType.getCutoutMipped());
+        RenderTypeLookup.setRenderLayer(BetterBadlandsBlocks.KINDLING_SLAB.get(), RenderType.getCutoutMipped());
+        RenderTypeLookup.setRenderLayer(BetterBadlandsBlocks.KINDLING_STAIRS.get(), RenderType.getCutoutMipped());
+        RenderTypeLookup.setRenderLayer(BetterBadlandsBlocks.KINDLING_VERTICAL_SLAB.get(), RenderType.getCutoutMipped());
     }
 }
