f781c5b629e800c42623f8c282024b0111ce268e
Sarinsa <mikkemusbruker@gmail.com>
2020-11-15 13:27:57 +0100

    I be silly goose and forgot to annotate with @SubscribeEvent

--- src/main/java/com/teamaurora/better_badlands/client/ClientRegister.java
+++ src/main/java/com/teamaurora/better_badlands/client/ClientRegister.java
@@ -1,12 +1,18 @@
 package com.teamaurora.better_badlands.client;
 
+import com.teamaurora.better_badlands.core.BetterBadlands;
 import com.teamaurora.better_badlands.core.registry.BetterBadlandsBlocks;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.RenderTypeLookup;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
+@Mod.EventBusSubscriber(value = Dist.CLIENT, modid = BetterBadlands.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ClientRegister {
 
+    @SubscribeEvent
     public static void registerClient(final FMLClientSetupEvent event) {
         setupRenderLayer();
     }
