2a3770b85b36b9f3a34f2b6ef15aa2292f041f7a
Sarinsa <mikkemusbruker@gmail.com>
2020-11-16 14:03:44 +0100

    Moved particle factory registration to ClientRegister

--- src/main/java/com/teamaurora/better_badlands/client/ClientRegister.java
+++ src/main/java/com/teamaurora/better_badlands/client/ClientRegister.java
@@ -1,10 +1,15 @@
 package com.teamaurora.better_badlands.client;
 
+import com.teamaurora.better_badlands.common.particle.TwigParticle;
 import com.teamaurora.better_badlands.core.BetterBadlands;
 import com.teamaurora.better_badlands.core.registry.BetterBadlandsBlocks;
+import com.teamaurora.better_badlands.core.registry.BetterBadlandsParticles;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.particle.ParticleManager;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.RenderTypeLookup;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.ParticleFactoryRegisterEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -22,5 +27,11 @@ public class ClientRegister {
         RenderTypeLookup.setRenderLayer(BetterBadlandsBlocks.KINDLING_SLAB.get(), RenderType.getCutoutMipped());
         RenderTypeLookup.setRenderLayer(BetterBadlandsBlocks.KINDLING_STAIRS.get(), RenderType.getCutoutMipped());
         RenderTypeLookup.setRenderLayer(BetterBadlandsBlocks.KINDLING_VERTICAL_SLAB.get(), RenderType.getCutoutMipped());
+    }
+
+    @SubscribeEvent
+    public static void registerParticles(ParticleFactoryRegisterEvent event) {
+        ParticleManager particleManagerIn = Minecraft.getInstance().particles;
+        particleManagerIn.registerFactory(BetterBadlandsParticles.TWIG.get(), TwigParticle.Factory::new);
     }
 }
