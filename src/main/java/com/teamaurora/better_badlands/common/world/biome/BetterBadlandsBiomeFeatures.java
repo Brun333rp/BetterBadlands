b2e22df9e452437ce18caa77e92d9c1ded271ec4
Epic312 <kaden_ostendorf@yahoo.com>
2020-12-15 18:49:53 -0700

    this and EM are gonna need me to make a PR to AC (read desc for more details) basically afaik the feature replacers can't actually replace features within features. so it works fine for a feature that's just directly spawning in the world (i.e. end gateways) but for stuff like trees and mushrooms, the biomes have features that contain the tree features within them. so you can't really replace those without parsing through the subfeatures within the features, and replacing all the subfeatures with what you want to replace. just replacing the big feature doesn't really work since the subfeatures aren't part of the feature, they're part of the config. i know this sounds like the missile knows where it is. it makes more sense in code i promise

--- src/main/java/com/teamaurora/better_badlands/common/world/biome/BetterBadlandsBiomeFeatures.java
+++ src/main/java/com/teamaurora/better_badlands/common/world/biome/BetterBadlandsBiomeFeatures.java
@@ -1,5 +1,7 @@
 package com.teamaurora.better_badlands.common.world.biome;
 
+import com.minecraftabnormals.abnormals_core.common.world.modification.BiomeFeatureModifier;
+import com.minecraftabnormals.abnormals_core.common.world.modification.BiomeModificationPredicates;
 import com.teamaurora.better_badlands.core.registry.BetterBadlandsFeatures;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
@@ -13,84 +15,18 @@ import net.minecraft.world.gen.placement.AtSurfaceWithExtraConfig;
 import net.minecraft.world.gen.placement.Placement;
 
 import java.util.ArrayList;
+import java.util.EnumSet;
 import java.util.List;
 
 public class BetterBadlandsBiomeFeatures {
-    /*public static BlockState DARK_OAK_LOG = Blocks.DARK_OAK_LOG.getDefaultState();
+    public static BlockState DARK_OAK_LOG = Blocks.DARK_OAK_LOG.getDefaultState();
     public static BlockState DARK_OAK_LEAVES = Blocks.DARK_OAK_LEAVES.getDefaultState();
 
     public static final BaseTreeFeatureConfig SMALL_DARK_OAK_CONFIG = (new BaseTreeFeatureConfig.Builder(new SimpleBlockStateProvider(DARK_OAK_LOG), new SimpleBlockStateProvider(DARK_OAK_LEAVES), null, null, null)).func_236700_a_().build();
 
-    public static void addSparseDarkOakTrees(Biome biomeIn) {
-        biomeIn.addFeature(GenerationStage.Decoration.VEGETAL_DECORATION, BetterBadlandsFeatures.SMALL_DARK_OAK_TREE.get().withConfiguration(SMALL_DARK_OAK_CONFIG).withPlacement(Placement.COUNT_EXTRA_HEIGHTMAP.configure(new AtSurfaceWithExtraConfig(5, 0.1F, 1))));
-    }
-
-    @SuppressWarnings("all")
-    public static void replaceOakTrees(Biome biome) {
-        List<ConfiguredFeature<?, ?>> list = biome.getFeatures(GenerationStage.Decoration.VEGETAL_DECORATION);
-        List<ConfiguredFeature<?, ?>> toRemove = new ArrayList<>();
-        int listSize = list.size();
-
-        for (int i = 0; i < listSize; i++) {
-            ConfiguredFeature<?, ?> configuredFeature = list.get(i);
-            if (configuredFeature.config instanceof DecoratedFeatureConfig) {
-                DecoratedFeatureConfig decorated = (DecoratedFeatureConfig) configuredFeature.config;
-                if (decorated.feature.config instanceof BaseTreeFeatureConfig && decorated.feature.feature instanceof TreeFeature) {
-                    BaseTreeFeatureConfig treeCfg = (BaseTreeFeatureConfig) decorated.feature.config;
-                    ConfiguredFeature<?, ?> cfgdTree = new ConfiguredFeature<>((TreeFeature)decorated.feature.feature, (BaseTreeFeatureConfig)decorated.feature.config);
-                    if (treeCfg == DefaultBiomeFeatures.OAK_TREE_CONFIG) {
-                        cfgdTree = BetterBadlandsFeatures.SMALL_DARK_OAK_TREE.get().withConfiguration(SMALL_DARK_OAK_CONFIG);
-                    }
-                    ConfiguredFeature<DecoratedFeatureConfig, ?> tempFeature = new ConfiguredFeature<>(
-                            (DecoratedFeature) configuredFeature.feature, new DecoratedFeatureConfig(
-                            cfgdTree, decorated.decorator
-                    ));
-                    biome.addFeature(GenerationStage.Decoration.VEGETAL_DECORATION, tempFeature);
-                    toRemove.add(configuredFeature);
-                }
-                else if (decorated.feature.config instanceof MultipleRandomFeatureConfig) {
-                    MultipleRandomFeatureConfig tree = (MultipleRandomFeatureConfig) decorated.feature.config;
-                    List<ConfiguredRandomFeatureList<?>> tempFeatures = new ArrayList<>();
-
-                    for (ConfiguredRandomFeatureList<?> featureList : tree.features) {
-                        if (featureList.feature.feature instanceof TreeFeature && featureList.feature.config == DefaultBiomeFeatures.OAK_TREE_CONFIG) {
-                            tempFeatures.add(new ConfiguredRandomFeatureList<>(BetterBadlandsFeatures.SMALL_DARK_OAK_TREE.get().withConfiguration(SMALL_DARK_OAK_CONFIG), featureList.chance));
-                        } else {
-                            tempFeatures.add(featureList);
-                        }
-                    }
-
-                    if (tree.defaultFeature.feature instanceof TreeFeature && tree.defaultFeature.config instanceof BaseTreeFeatureConfig) {
-                        BaseTreeFeatureConfig tempDefCfg = (BaseTreeFeatureConfig) tree.defaultFeature.config;
-                        Feature<BaseTreeFeatureConfig> tempDef2 = (Feature<BaseTreeFeatureConfig>) tree.defaultFeature.feature;
-                        //if (((BaseTreeFeatureConfig) tree.defaultFeature.config).leavesProvider.getBlockState(probeRand, probePos).getBlock() == Blocks.OAK_LEAVES) {
-                        BaseTreeFeatureConfig treeCfg = (BaseTreeFeatureConfig) tree.defaultFeature.config;
-                        if (treeCfg == DefaultBiomeFeatures.OAK_TREE_CONFIG) {
-                            tempDefCfg = SMALL_DARK_OAK_CONFIG;
-                            tempDef2 = BetterBadlandsFeatures.SMALL_DARK_OAK_TREE.get();
-                        }
-                        ConfiguredFeature<?, ?> tempDef = new ConfiguredFeature<>(tempDef2, tempDefCfg);
-                        ConfiguredFeature<DecoratedFeatureConfig, ?> tempFeature = new ConfiguredFeature<>(
-                                (DecoratedFeature) configuredFeature.feature, new DecoratedFeatureConfig(
-                                new ConfiguredFeature<>((Feature<MultipleRandomFeatureConfig>) decorated.feature.feature,
-                                        new MultipleRandomFeatureConfig(tempFeatures, tempDef)
-                                ), decorated.decorator
-                        ));
-                        biome.addFeature(GenerationStage.Decoration.VEGETAL_DECORATION, tempFeature);
-                    } else {
-                        ConfiguredFeature<DecoratedFeatureConfig, ?> tempFeature = new ConfiguredFeature<>(
-                                (DecoratedFeature) configuredFeature.feature, new DecoratedFeatureConfig(
-                                new ConfiguredFeature<>((Feature<MultipleRandomFeatureConfig>) decorated.feature.feature,
-                                        new MultipleRandomFeatureConfig(tempFeatures, tree.defaultFeature)
-                                ), decorated.decorator
-                        ));
-                        biome.addFeature(GenerationStage.Decoration.VEGETAL_DECORATION, tempFeature);
-                    }
-                    toRemove.add(configuredFeature);
-                }
-            }
-        }
-        toRemove.forEach(list::remove);
-    }*/
-    // TODO: fix worldgen
+    public static final BiomeFeatureModifier BADLANDS_TREE_MODIFIER = BiomeFeatureModifier.createFeatureReplacer(
+            BiomeModificationPredicates.forCategory(Biome.Category.MESA),
+            EnumSet.of(GenerationStage.Decoration.VEGETAL_DECORATION),
+            ()->Feature.TRE
+    )//fuck
 }
